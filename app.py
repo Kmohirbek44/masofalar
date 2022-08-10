ebacc53dac841495afb0b781e8ed6b700718a86b
Kmohirbek44 <77918438+Kmohirbek44@users.noreply.github.com>
2022-08-10 17:27:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import config
 from items import item
 async def on(dp):
     await item.set(dp)
-    requests.post(f"https://api.telegram.org/bot{config.token}/sendMessage?chat_id={config.ip}&text=Online")
+    requests.post(f"https://api.telegram.org/bot{config.token}/sendMessage?chat_id={config.ip}&text=Qayum nima bo'ldi pastigingda pulin bormi")
 executor.start_polling(dp,on_startup=on)
