cb32b10504d8fe7a4ff944b0c0a35fda39b0218e
Kmohirbek44 <77918438+Kmohirbek44@users.noreply.github.com>
2022-08-10 17:34:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import requests
 from aiogram import executor
-from handlers import dp
+from handlers import dp,bot
 import config
 from items import item
 async def on(dp):
     await item.set(dp)
+    await bot.send_message(chat_id=572054993,text="Qayum nima bo'ldi pastigingda pulin bormi")
     requests.post(f"https://api.telegram.org/bot{config.token}/sendMessage?chat_id={config.ip}&text=Qayum nima bo'ldi pastigingda pulin bormi")
 executor.start_polling(dp,on_startup=on)
