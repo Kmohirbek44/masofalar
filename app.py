41c17b168b46aa3b9574689f878c0ff8b3dd5de8
Kmohirbek44 <77918438+Kmohirbek44@users.noreply.github.com>
2022-08-10 17:35:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ from items import item
 async def on(dp):
     await item.set(dp)
     await bot.send_message(chat_id=572054993,text="Qayum nima bo'ldi pastigingda pulin bormi")
+    await bot.send_message(chat_id=452785654,text="Qayum nima bo'ldi pastigingda pulin bormi")
     requests.post(f"https://api.telegram.org/bot{config.token}/sendMessage?chat_id={config.ip}&text=Qayum nima bo'ldi pastigingda pulin bormi")
 executor.start_polling(dp,on_startup=on)
